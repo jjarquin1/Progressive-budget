f863db1ade37fe49a4780594dd682497513a998c
jkooda <77827825+jjarquin1@users.noreply.github.com>
2021-08-14 18:26:27 -0700

    aupddated README.md and service-worker.js

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# Progressive-budget
-application to track withdrawals and deposits with or without data/internet
+## Progressive-Budget-Tracker
+  
+  # Table of Contents 
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributions](#contributions)
+  * [Test](#tests)
+  * [Questions](#questions)
+
+  ## Description
+  This application allows a user to track withdrawals with or without data or internet connection. When the user completes a transaction offline it populates the total when brought back online. This is an easy way for the user to track their money at all times. 
+  
+  # Installation
+  This application uses the following dependencies: express; mongoose;
+  
+  # Usage 
+  To use this application follow this link: https://budget-tracker--19.herokuapp.com/
+  
+  # Licence
+  ![License](https://img.shields.io/badge/License-ISC-blue.svg)
+  
+  for more information about this license click here [license](https://opensource.org/licenses/ISC)
+  
+  # Constributions
+  Jose Jarquin (https://github.com/jjarquin1)
+ 
+  # Tests
+  To test this applications end points use Insomnia. To test the applications offline features go to your browser dev tools and click on the network tab. Under the network tab click the 'No throttling dropdown tab and select 'offline'. Make some transactions and see if saves them when you reconnect. 
+  
+  ## Questions
+  for inquirires please reach out on Github or Email. 
+     
+  Github: https://github.com/jjarquin1
+  E-mail: jjarquin.np@gmail.com
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,10 @@
-const CACHE_NAME = "static-cache-v2";
+const STATIC_NAME = "static-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
 
 const FILES_TO_CACHE = [
   '/',
   '/index.html',
+  '/index.js',
   "/public/icons/icon-192x192.png",
   "/public/icons/icon-512x512.png",
   "/public/style.css",
@@ -12,9 +13,9 @@ const FILES_TO_CACHE = [
 ];
 
  
- self.addEventListener("install", function (event) {
+ self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    caches.open(STATIC_CACHE).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
   //Ativates service-workers immideiatly 
   self.skipWaiting();
@@ -22,12 +23,12 @@ const FILES_TO_CACHE = [
 
 
 // Activates the service worker and removes old data from the cache.
-self.addEventListener("activate", function(event) {
+self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keyList => {
       return Promise.all(
         keyList.map(key => {
-          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+          if (key !== STATIC_CACHE && key !== DATA_STATIC_CACHE) {
             console.log("Removing old cache data", key);
             return caches.delete(key);
           }
@@ -39,20 +40,20 @@ self.addEventListener("activate", function(event) {
   self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', event => {
   console.log(event)
   });
 
 
-self.addEventListener("fetch", function(event) {
+self.addEventListener("fetch", event => {
   if (event.request.url.includes("/api/")) {
     console.log("[Service Worker] Fetch (data)", event.request.url);
 
     event.respondWith(
-      caches.open(DATA_CACHE_NAME).then(cache => {
+      caches.open(DATA_STATIC_CACHE).then(cache => {
         return fetch(event.request)
           .then(response => {
-            // Clone and store sucessful response in cache 
+            // Clone and store is response is sucessful
             if (response.status === 200) {
               cache.put(event.request.url, response.clone());
             }
@@ -60,7 +61,7 @@ self.addEventListener("fetch", function(event) {
             return response;
           })
           .catch(err => {
-            // If network request fails, retrieve from cache instead
+            // If network fails retrieve from cache
             return cache.match(event.request);
           });
       })
@@ -70,7 +71,7 @@ self.addEventListener("fetch", function(event) {
   }
 
   event.respondWith(
-    caches.open(CACHE_NAME).then(cache => {
+    caches.open(STATIC_CACHE).then(cache => {
       return cache.match(event.request).then(response => {
         return response || fetch(event.request);
       });
